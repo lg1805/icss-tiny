1c88ccb9c12a481343b1471e357abcded359334a
Lakshya Rubi Gnanasambanthan <161871017+lg1805@users.noreply.github.com>
2025-04-28 13:54:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -376,6 +376,5 @@ def upload_file():
     return send_file(processed, as_attachment=True)
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT env variable, default to 5000
     app.run(debug=True, host='0.0.0.0', port=port)
-
